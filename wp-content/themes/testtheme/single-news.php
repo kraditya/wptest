1038fa7b8b0eaff50c518048035eb958d12638c8
Kripanath Aditya <kraditya2006@gmail.com>
2021-01-17 12:58:42 +0600

    Custom fields update

--- wp-content/themes/testtheme/single-news.php
+++ wp-content/themes/testtheme/single-news.php
@@ -2,6 +2,7 @@
 
 /* 
 Template Name: News
+Template Post Type: news
 */
 
 
@@ -13,17 +14,49 @@ get_header();
 
 <!-- Show the contents -->
 
+<?php 
+    while (have_posts()) {
+        the_post();
+        
+        ?><h3><?php the_title(); ?></h3><?php
+
+        ?>
+
+        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
+
+        <?php 
+        
+        the_content();        
+
+        echo "<br/>";
+
+        echo 'Posted by ';
+        the_author();       
+        
+        comment_form();
+    }
+?>
+
 <?php
 
-while (have_posts()) {
-	the_post();
 
-	?>
-	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	<?php
+/* ------------------------------- Options ------------------------------ */
+$color = get_field('choose_your_color');
+echo"<b>Color: </b>".$color['value'];
+echo"<br>";
+echo"<br>";
 
-	the_excerpt();
-}
+/* ------------------------------- check boxes ------------------------------ */
+$subject = get_field('choose_your_subject');
+echo"<b>Subject: </b>".implode(', ', $subject);
+echo"<br>";
+echo"<br>";
+
+/* ------------------------------- radio button ------------------------------ */
+$do_you_agree = get_field('do_you_agree');
+echo"<b>Do you agree: </b>".$do_you_agree;
+echo"<br>";
+echo"<br>";
 
 ?>
 
